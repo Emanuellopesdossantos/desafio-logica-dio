d66bf5c4916441bab1e600119f26437f14696440
Emanuellopesdossantos <Lopesemanuel082@gmail.com>
2023-11-29 11:46:55 -0300

    Projeto

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-console.log("Hello, World!");
+
+let nomeHeroi = "Emanuel"
+let xp = 50000
+
+//ferro
+if (xp <= 1000) {
+    console.log("O Herói de nome " + nomeHeroi + " está no nível de Ferro");
+}
+//Bronze
+if (xp >= 1001 & xp <= 2000 ) {
+    console.log("O Herói de nome " + nomeHeroi + " está no nível de Bronze");
+}
+
+//Prata
+if (xp >= 2001 & xp <= 5000 ) {
+    console.log("O Herói de nome " + nomeHeroi + " está no nível de Prata");
+}
+
+//Ouro
+if (xp >= 6001 & xp <= 7000 ) {
+    console.log("O Herói de nome " + nomeHeroi + " está no nível de Ouro");
+}
+
+//Platina
+if (xp >= 7001 & xp <= 8000 ) {
+    console.log("O Herói de nome " + nomeHeroi + " está no nível de Platina");
+}
+
+//Ascendente
+if (xp >= 8001 & xp <= 9000 ) {
+    console.log("O Herói de nome " + nomeHeroi + " está no nível de Ascendente");
+}
+
+//Imortal
+if (xp >= 9001 & xp <= 10000 ) {
+    console.log("O Herói de nome " + nomeHeroi + " está no nível de Imortal");
+}
+
+//Radiante
+if (xp >= 10001 ) {
+    console.log("O Herói de nome " + nomeHeroi + " está no nível de Radiante");
+}
